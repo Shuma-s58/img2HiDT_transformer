3a0d9b61fbf572216f4eaa5f119e46c79d7b902a
Shuma-s58 <shuma.suzuki.2021@outlook.jp>
2025-02-22 17:40:37 +0900

    create ROS node

--- src/script/HiDT_transformer.py
+++ src/script/HiDT_transformer.py
@@ -1,3 +1,71 @@
 # !/usr/bin/env python3
 
+from pathlib import Path
 
+### HiDT ###
+import cv2
+import numpy as np
+import time
+
+import sys
+sys.path.append(str(Path.home()) + '/catkin_ws/src/HiDT')
+
+import torch
+import PIL.Image as PILImage
+from torchvision import transforms
+
+from hidt.networks.enhancement.RRDBNet_arch import RRDBNet
+from hidt.style_transformer import StyleTransformer
+from hidt.utils.preprocessing import GridCrop, enhancement_preprocessing
+### HiDT ###
+
+class HiDT_transformer():
+    def __init__(self):
+        common_path = str(Path.home()) + '/catkin_ws/src/HiDT'
+        self.config_path = common_path + '/configs/daytime.yaml'
+        self.gen_weights_path = common_path + '/trained_models/generator/daytime.pt'
+        self.inference_size = 256  # the network has been trained to do inference in 256px, any higher value might lead to artifacts
+        self.device = 'cuda:0'
+        self.image_path = common_path + '/images/daytime/content/1.jpg'
+        self.styles_path = common_path + '/styles.txt'
+        self.enhancer_weights = common_path + '/trained_models/enhancer/enhancer.pth'
+
+        self.style_transformer = StyleTransformer(self.config_path,
+                                                  self.gen_weights_path,
+                                                  inference_size=self.inference_size,
+                                                  device=self.device)
+
+        with open(self.styles_path) as f:
+            styles = f.read()
+        styles = {style.split(',')[0]: torch.tensor([float(el) for el in style.split(',')[1][1:-1].split(' ')]) for style in styles.split('\n')[:-1]}
+
+        self.style_to_transfer = styles['sunsetred']
+        self.style_to_transfer = self.style_to_transfer.view(1, 1, 3, 1).to(self.device)
+
+
+    def HiDT(self, img):
+
+        # PIL 形式に変換
+        image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+        with torch.no_grad():
+            content_decomposition = self.style_transformer.get_content(img)[0]
+            decoder_input = {'content': content_decomposition['content'],
+                            'intermediate_outputs': content_decomposition['intermediate_outputs'],
+                            'style': self.style_to_transfer}
+            transferred = self.style_transformer.trainer.gen.decode(decoder_input)['images']
+
+        output_frame = transforms.ToPILImage()((transferred[0].cpu().clamp(-1, 1) + 1.) / 2.)
+
+        # Pillow の Image オブジェクトを NumPy 配列に変換 (OpenCV 互換の形式に)
+        image_cv = np.array(output_frame)
+        # Pillow は RGB 形式なので、OpenCV の BGR に変換 (必要に応じて)
+        image_cv = cv2.cvtColor(image_cv, cv2.COLOR_RGB2BGR)
+
+        return image_cv
+
+
+if __name__ == '__main__':
+    transformer = HiDT_transformer()
+    #img = PILImage.open('/home/shuma/catkin_ws/src/HiDT/images/daytime/content/1.jpg')
+    #transformer.HiDT(img)
--- src/script/img2HiDT_node.py
+++ src/script/img2HiDT_node.py
@@ -1,1 +1,85 @@
-#!/use/bin/env python3
+#!/usr/bin/env python3
+
+import roslib
+roslib.load_manifest('nav_cloning')
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from HiDT_transformer import *
+
+class img2HiDT_node:
+    def __init__(self):
+        rospy.init_node('img2HiDT_node', anonymous=True)
+        self.bridge = CvBridge()
+        self.H_trans = HiDT_transformer()
+
+        ### zero array ###
+        self.cv_image = np.zeros((480,640,3), np.uint8)
+        self.cv_left_image = np.zeros((480,640,3), np.uint8)
+        self.cv_right_image = np.zeros((480,640,3), np.uint8)
+        self.HiDT_image = self.bridge.cv2_to_imgmsg(self.cv_image, encoding="rgb8")
+        self.HiDT_left_image = self.bridge.cv2_to_imgmsg(self.cv_left_image, encoding="rgb8")
+        self.HiDT_right_image = self.bridge.cv2_to_imgmsg(self.cv_right_image, encoding="rgb8")
+
+        ### Subscriber ###
+        self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.callback_camera)
+        self.image_left_sub = rospy.Subscriber("/camera_left/rgb/image_raw", Image, self.callback_left_camera)
+        self.image_right_sub = rospy.Subscriber("/camera_right/rgb/image_raw", Image, self.callback_right_camera)
+
+        ### Publisher ###
+        self.image_pub = rospy.Publisher('/hidt_camera/rgb/image_raw', Image, queue_size=1)
+        self.image_left_pub = rospy.Publisher('/hidt_camera_left/rgb/image_raw', Image, queue_size=1)
+        self.image_right_pub = rospy.Publisher('/hidt_camera_right/rgb/image_raw', Image, queue_size=1)
+
+    def callback_camera(self, data):
+        try:
+            self.cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            self.HiDT_image = img2HiDT(self.cv_image)
+            #self.image_pub.publish(self.HiDT_image)
+        except CvBridgeError as e:
+            print(e)
+
+    def callback_left_camera(self, data):
+        try:
+            self.cv_left_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            self.HiDT_left_image = img2HiDT(self.cv_left_image)
+            #self.image_left_pub.publish(self.HiDT_left_image)
+        except CvBridgeError as e:
+            print(e)
+
+    def callback_right_camera(self, data):
+        try:
+            self.cv_right_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            self.HiDT_right_image = img2HiDT(self.cv_right_image)
+            #self.image_right_pub.publish(self.HiDT_right_image)
+        except CvBridgeError as e:
+            print(e)
+
+    def img2HiDT(self, image):
+        #trans_image = np.zeros((480,640,3), np.uint8)
+
+        if self.image.size != 640 * 480 * 3:
+            return
+
+        trans_image = self.H_trans.HiDT(image)
+
+        # numpy配列をROSのImageメッセージに変換
+        trans_image = self.bridge.cv2_to_imgmsg(trans_image, encoding="rgb8")
+
+        return trans_image
+
+    def loop(self):
+        self.image_pub.publish(self.HiDT_image)
+        self.image_left_pub.publish(self.HiDT_left_image)
+        self.image_right_pub.publish(self.HiDT_right_image)
+
+
+if __name__ == '__main__':
+    node = img2HiDT_node()
+
+    DURATION = 0.2
+    r = rospy.Rate(1 / DURATION)
+    while not rospy.is_shutdown():
+        node.loop()
+        r.sleep()
